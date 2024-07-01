b54c3d3390a5260d75c74f5db8fb9c69755bb80c
Даня <lavenov.danila@inbox.ru>
2024-07-01 20:20:12 +0300

    Task-test

--- homework_01/main.py
+++ homework_01/main.py
@@ -1,12 +1,19 @@
+numbers = (1, 3, 5, 7)
+
+
+# filter types
+ODD = "odd"
+EVEN = "even"
+PRIME = "prime"
+
+
 #Функция возведения элементов чписка в квадрат
 def power_numbers(*numbers):
     return [i ** 2 for i in numbers]
 print(power_numbers())
 
+
 #Фильтрация чисел
-numbers = (1, 3, 5, 7)
-
-
 def is_prime(n):
     if n < 2:
         return False
@@ -14,10 +21,6 @@ def is_prime(n):
         if n % i == 0:
             return False
     return True
-# filter types
-ODD = "odd"
-EVEN = "even"
-PRIME = "prime"
 
 
 def filter_numbers(numbers, filter_type):
@@ -27,6 +30,7 @@ def filter_numbers(numbers, filter_type):
         return [num for num in numbers if num % 2 == 0]
     elif filter_type ==PRIME:
         return [num for num in numbers if is_prime(num)]
+
 
 print(filter_numbers(numbers, ODD))
 print(filter_numbers(numbers, EVEN))
