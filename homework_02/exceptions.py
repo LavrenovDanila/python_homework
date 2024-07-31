b1c8276e8bd468c55adf89f621d0e32f33760849
Даня <lavenov.danila@inbox.ru>
2024-07-31 20:58:00 +0300

    HW№2

--- homework_02/exceptions.py
+++ homework_02/exceptions.py
@@ -1,6 +1,13 @@
-"""
-Объявите следующие исключения:
-- LowFuelError
-- NotEnoughFuel
-- CargoOverload
-"""
+
+
+class LowFuelError(Exception):
+    "Исключение, возникающее при недостаточном уровне топлива для старта."
+    pass
+
+class NotEnoughFuel(Exception):
+    "Исключение, возникающее при недостаточном топливе для движения."
+    pass
+
+class CargoOverload(Exception):
+    "Исключение, возникающее при перегрузке грузоподъемности."
+    pass
