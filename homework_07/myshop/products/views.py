3523cb9e321e1d3ebe512d5a1e08a7a86676b757
Даня <lavenov.danila@inbox.ru>
2024-11-30 13:56:42 +0300

    HW№8

--- homework_07/myshop/products/views.py
+++ homework_07/myshop/products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from django.views.generic import ListView, DetailView
+from .models import Product, Category
 
-# Create your views here.
+class ProductListView(ListView):
+    model = Product
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+    paginate_by = 10  # Вы можете изменить это число, если нужно
+
+    def get_queryset(self):
+        return Product.objects.filter(available=True)
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
